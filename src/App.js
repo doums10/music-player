1afc516cd8067696b21e422436537a1c794dc510
mamadou sylla <m27sylla@gmail.com>
2020-12-13 12:34:10 +0100

    paramétrage forward et backward done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Player from "./components/Player";
-import PlayerControls from "./components/PlayerControls";
 
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "Is This Love",
       artist: "Bob Marley",
@@ -34,11 +33,23 @@ function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if(currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  },[currentSongIndex]);
+
   return (
     <div className="App">
       <Player 
-      song={songs[currentSongIndex]} 
-      nextSong={songs[nextSongIndex]} />
+      currentSongIndex={currentSongIndex}
+      setCurrentSongIndex={setCurrentSongIndex}
+      nextSongIndex={nextSongIndex}
+      songs={songs} />
       
     </div>
   );
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React { useEffect, useRef, useState } from "react";
 import PlayerDetails from "./PlayerDetails";
 import PlayerControls from "./PlayerControls";
 
 function Player(props) {
+
+  const audioEl = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  //paramétrage lecture des sons
+  useEffect(() => {
+    if (isPlaying) {
+      audioEl.current.play();
+    } else {
+      audioEl.current.pause();
+    }
+  });
+
+  //paramétrage changement de sons forward
+  const SkipSong = (forwards = true) => {
+    if(forwards) {
+     props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+
+        if (temp > songs.length - 1) {
+          temp = 0;
+        }
+        return temp;
+     });
+     //paramétrage backward
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+        return temp;
+      });
+    }
+  }
+  
   return (
     <div className="c-player">
-      <audio src=""></audio>
+      <audio src={props.songs[props.currentSongIndex].src} ref={audioEl}></audio>
       <h4>Playing Now</h4>
       {/*DETAILS*/}
-      <PlayerDetails song={props.song} />
+      <PlayerDetails song={props.songs[props.currentSongIndex]} />
       {/*CONTROLS*/}
 						<PlayerControls />
       <p>
