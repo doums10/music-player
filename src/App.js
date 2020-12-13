78f067854414796f3220d27577b94bb587504b28
mamadou sylla <m27sylla@gmail.com>
2020-12-13 10:37:48 +0100

    css done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Player from "./components/Player";
 import PlayerControls from "./components/PlayerControls";
 
